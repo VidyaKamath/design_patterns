3c39d37dc9ab0ab66c71e7bb6f69a1216258b3e8
Vidya Kamath P <42544705+VidyaKamath@users.noreply.github.com>
2023-12-01 16:34:14 -0600

    Update client.py
    Add abstarct factory

--- client.py
+++ client.py
@@ -1,26 +1,25 @@
-from factory import IProductRelease, ProductReleaseFactory
+from factory import IProductReleaseFactory, ProductReleaseFactory_1, ProductReleaseFactory_2
 
-def publish_release_info(product_release_info: IProductRelease):
-	release_date = product_release_info.get_release_date()
-	sw_release_version = product_release_info.get_sw_release_version()
-	release_type = product_release_info.get_release_type()
-	print(f"***** Product {product_release_info.product_type} *******")
-	print(f"Release date: {release_date}")
-	print(f"Release Version: {sw_release_version}")
-	print(f"Release Type: {release_type}\n")
-
-
-def notify_client(product_type):
+def notify_client_to_publish(product_release_factory: IProductReleaseFactory, product_type):
 	# Scenario 1 - Factory Pattern
 	# Intent is to seperate the responsibility of object creation from the client
 
 	# Adding new products will not impact the client code 
-	# Creating the Product release object for a given product type is delegated to the Factory class.
+	# Creating the Product release object for a given factory and 
+	# product type is delegated to the Factory class.
 
-	product_release_factory = ProductReleaseFactory(product_type)
-	product_release_info = product_release_factory.get_product_release()
-	publish_release_info(product_release_info)
+	product_release_info = product_release_factory.get_product_release(product_type)
+	product_release_info.publish_release_info()
 
 if __name__ == "__main__":
-	notify_client(product_type="A")
-	notify_client(product_type="B")
+	# suppose the client gets notified about the publish of the 
+
+	notify_client_to_publish(ProductReleaseFactory_1(), "A")
+	notify_client_to_publish(ProductReleaseFactory_1(), "B")
+	notify_client_to_publish(ProductReleaseFactory_1(), "A1")
+	notify_client_to_publish(ProductReleaseFactory_1(), "B1")
+
+	notify_client_to_publish(ProductReleaseFactory_2(), "A")
+	notify_client_to_publish(ProductReleaseFactory_2(), "B")
+	notify_client_to_publish(ProductReleaseFactory_2(), "A2")
+	notify_client_to_publish(ProductReleaseFactory_2(), "B2")
